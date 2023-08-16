507f7723bfb613e72bde2aa9fb70dc8d6865fe41
swethakeerthipati <swethakeerthipati@gmail.com>
2023-08-16 09:37:36 +0200

    Add Application

---
+++ src/components/Weather.js
@@ -0,0 +1,34 @@
+const API_KEY = "0eae3f24245f304fb1a8163a69a8fa6b";
+const makeIconURL = (iconId) =>
+  `https://openweathermap.org/img/wn/${iconId}@2x.png`;
+const getFormattedWeatherData = async (city, units = "metric") => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+  const data = await fetch(URL)
+    .then((response) => response.json())
+    .then((data) => data);
+  const {
+    weather,
+    main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+    wind: { speed },
+    sys: { country },
+    name,
+  } = data;
+
+  const { description, icon } = weather[0];
+
+  return {
+    description,
+    iconURL: makeIconURL(icon),
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    pressure,
+    humidity,
+    speed,
+    country,
+    name,
+  };
+};
+
+export { getFormattedWeatherData };
